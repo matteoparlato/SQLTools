950373bc4745abf0abc1667f608693eb90f48744
Matteo Parlato <matteo.parlato@outlook.it>
2020-02-18 11:18:36 +0100

    Create DeleteUsers.sql

---
+++ DeleteUsers.sql
@@ -0,0 +1,14 @@
+DELETE
+FROM [dbo].[User]
+DELETE
+FROM [dbo].[Access Control]
+DELETE
+FROM [dbo].[User Property]
+DELETE
+FROM [dbo].[Page Data Personalization]
+DELETE
+FROM [dbo].[User Default Style Sheet]
+DELETE
+FROM [dbo].[User Metadata]
+DELETE
+FROM [dbo].[User Personalization]
